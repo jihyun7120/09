a87331115d4bf8e8871c89dd350a42b1c74b1b41
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-30 18:18:02 +0900

    practice3_2

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5 
+#define SIZE 5
 
 int main(int argc, char *argv[])
 {
+    
+    int a[SIZE]={1,2,3,4,5};
+    int b[SIZE]={1,2,3,4,5};
     int i;
-    int grade[SIZE];
-    int score[SIZE];
+    
+    int flag_same = 1;
     
     for(i=0;i<SIZE;i++)
     {
-        grade[i]=rand()%100;//0-99¹«ÀÛÀ§  
-        score[i]=grade[i];
+        if(a[i]!=b[i])
+        {
+            printf("%i-th element is diff", i);
+            flag_same= 0;
+        }
 }
-        
-    for(i=0;i<SIZE;i++)
-        printf("grade[%d]=%d, score[%d]=%d\n", i, grade[i], i, score[i]);
+
+    printf("Are a and b the same?%i\n", flag_same);
+    
     
   system("PAUSE");	
   return 0;
